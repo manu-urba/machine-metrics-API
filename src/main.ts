07f1b3fa8a7a0b76328d06a5aaf7f5192647dc37
Manuel <manuel@manuel.ws>
2023-08-15 13:24:06 +0200

    :memo: Implements swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+  const config = new DocumentBuilder()
+    .setTitle('Mavarick Machine Matrix API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('help', app, document);
   await app.listen(port);
 }
 bootstrap();
